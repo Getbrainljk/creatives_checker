8b4f7ab84f97bdb3c7ddbb7a8c98452141db0b34
Nadir Arbia <nadir.arbia@gmail.com>
2020-10-22 11:47:06 +0200

    up

--- src/index.js
+++ src/index.js
@@ -9,18 +9,26 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import { DropzoneDialog } from 'material-ui-dropzone';
 
-import specsProgDisplay  from  './data_creatives-specs/specs-progDisplay.json'
-import specsProgVideo  from  './data_creatives-specs/specs-progVideo.json'
 
+const specsProgDisplay = require('./data_creatives-specs/specs-progDisplay.json')
+const specsProgVideo = require('./data_creatives-specs/specs-progVideo.json')
+
+var inOrder = new Boolean(true)
 
 
 class CreaChecker extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-        }
-    }
 
+        };
+        const validationMessages = {
+            successMsg: () => 'Les créatives sont bien aux formats!',
+            noteMsg: () => 'Ici joint les formats et specificités liés à vos creatives:',
+            formatMsg: (param) => `Le format'+ ${param.requiredLength}`,
+            extensionMsg: (param) => `Max chars allowed is ${param.requiredLength}`,
+        };
+    }
 
     render() {
         let isValid;
@@ -32,6 +40,8 @@ class CreaChecker extends React.Component {
                     Attention: Cette verification comporte l’extension, la taille, le poid, les normes basique mais n’incluent pas les normes de visibilité.</h6>
                 <div className="mb-3">
                     <DropzoneDialogElem />
+                </div>
+                <div>
                 </div>
             </Container>
         );
@@ -49,15 +59,84 @@ export default class DropzoneDialogElem extends React.Component {
                 width: PropTypes.string,
                 height: PropTypes.number,
                 filesObj: null,
+                inOrder: true,
             },
             creas2: []
+
         };
-        this.handleOnDrop = this.handleOnDrop.bind(this);
     }
+
+    /*
+    taillesRecommandes
+    normesRespecter
+    poidsMaximum
+    extensionsAutorises
+    */
+
+
     componentDidUpdate(prevProps) {
+
+        function filterByContains(jsonObj, id, param) {
+            if (!jsonObj || !param)
+                return false;
+            for (let i = 0; i < jsonObj.length; i += 1) {
+                if (jsonObj[i] && (id in jsonObj[i]))
+                {
+                    console.log("jsonobj i id:" +jsonObj[i][id].toString().toLowerCase());
+                    console.log("param:" + param.toString().toLowerCase());
+                    if ((jsonObj[i][id].toString().toLowerCase()
+                                .includes(param.toString().toLowerCase())) == true)
+                                return true;
+            }}
+            return false;
+        }
+        function formatsChecker(crea) {
+       //     for (let i = 0; i < this.state.creas2.length; i += 1) {
+                var inOrder = true;
+                const sizeCreasInput = crea.width.toString() + "x" + crea.height.toString();
+                for (let i = 0; i < specsProgDisplay.length; i += 1) {
+                    if (specsProgDisplay[i].taillesRecommandes) {
+                        if (specsProgDisplay[i].taillesRecommandes.toString().toLowerCase().includes(sizeCreasInput)) {
+                            inOrder = true;
+                            crea.inOrder = true;
+                            console.log("aux specs (formats)");
+                            break;
+                        }
+                    }
+                    inOrder = false;
+                }
+                if (!inOrder) {
+                    crea.inOrder = false;
+                    console.log("pas aux specs (formats)");
+                    return;//break;
+                }
+           // }
+        }
         console.log("================  component did update ==================")
-  
+        for (let i = 0; i < this.state.creas2.length; i += 1) {
+
+            var ext = this.state.creas2[i].filesObj.type.substr(this.state.creas2[i].filesObj.type.lastIndexOf('/') + 1);
+
+            //      for (let i = 0; i < specsProgDisplay.length; i += 1) {
+            //  if (!specsProgDisplay[i].extensionsAutorises)
+            //    continue;
+//            console.log("bool?:"+filterByContains(specsProgDisplay, "extensionsAutorises", ext));
+            switch (true) {
+
+                case filterByContains(specsProgDisplay, "extensionsAutorises", ext):
+                    console.log("IMG FILE DETECTED");
+                    formatsChecker(this.state.creas2[i]);
+                    break;
+                case filterByContains(specsProgVideo, "extensionsAutorises", ext):
+                    console.log("VIDEO FILE DETECTED");
+                    break;
+                default:
+                    console.log(" pas aux specs?: extension:" + ext);
+                    break;
+            }
+        }
     }
+
     handleClose() {
         this.setState({
             open: false
@@ -66,29 +145,32 @@ export default class DropzoneDialogElem extends React.Component {
     }
 
     handleSave(files) {
+        console.log("================  OnSave() ==================")
+
         //Saving files to state for further use and closing Modal.
         this.setState({
             files: files,
             open: false
         });
-        console.table(this.state.creas2);
-        console.table(specsProgDisplay);
-        console.table(specsProgVideo);
+
     }
     handleOpen() {
         this.setState({
             open: true,
         })
+        console.log("handleOpen");
     }
+
     handleOnDrop(files) {
+        console.log("================  OnDrop() ==================")
+
         this.setState({
             files: files,
             open: true,
         })
-        for (let i = 0; i < files.length; i += 1) {
-            console.log("================ for loop ==================")
 
-            // console.log(files[i]);
+        for (let i = 0; i < files.length; i += 1) {
+
             const oFReader = new FileReader();
             oFReader.readAsDataURL(files[i]);
             oFReader.onload = (e) => {
@@ -106,13 +188,29 @@ export default class DropzoneDialogElem extends React.Component {
                             ]
                         }
                     })
+                    image = null;
                 }
+                if (image) {
+                    this.setState(prevState => {
+                        return {
+                            creas2: [...prevState.creas2,
+                            {
+                                width: image.width,
+                                height: image.height,
+                                filesObj: files[i],
+                            },
+                            ]
+                        }
+                    })
+                }
+
             }
         }
     }
 
     render() {
         return (
+
             <div>
 
                 <Button variant="contained" onClick={this.handleOpen.bind(this)} color="primary">
@@ -121,9 +219,9 @@ export default class DropzoneDialogElem extends React.Component {
                 <DropzoneDialog
                     //  dialogTitle
                     open={this.state.open}
-                     onSave={this.handleSave.bind(this)}
-                    acceptedFiles={['image/jpeg', 'image/png', 'image/jpg', 'image/gif']}
-                    showPreviews={true}
+                    onSave={this.handleSave.bind(this)}
+                    //   acceptedFiles={['image/jpeg', 'image/png', 'image/jpg', 'image/gif']}
+                    showPreviews={false}
                     onClose={this.handleClose.bind(this)}
                     // onAdd={this.handleOnAdd.bind(this)}
                     // onChange={this.handleOnChange.bind(this)}
@@ -137,16 +235,6 @@ export default class DropzoneDialogElem extends React.Component {
         );
     }
 }
-function creativesChecker(creas) {
-
-    for (let i = 0; i < creas.length; i += 1) {
-        console.log(creas[i].width);
-        console.log(creas[i].height);
-        console.log(creas[i].files[i]);
-    }
-    return true;
-}
-
 
 
 ReactDOM.render(
